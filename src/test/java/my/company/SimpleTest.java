9d757e6dcd1ce95a4933476bc92cd5f60ac5cbf8
Evgeniyli <Evgeniyli@users.noreply.github.com>
2023-05-31 13:32:33 +0000

    Automated deployment: Wed May 31 13:32:33 UTC 2023 e1e10166c1995b44110e668fcbe57c1748ab9be3

---
+++ src/test/java/my/company/SimpleTest.java
@@ -0,0 +1,40 @@
+package my.company;
+
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+/**
+ * @author baev (Dmitry Baev)
+ */
+public class SimpleTest {
+
+    @Test
+    public void simpleTestOne() {
+        step("step 1");
+    }
+    @Test
+    public void simpleTestTwo() {
+        step("step 1");
+        step("step 2");
+    }
+
+    @Test
+    public void simpleTestThree() {
+        step("step 4");
+        step("step 55");
+    }
+
+    @Test
+    public void simpleTestFourth() {
+        step("step 77777");
+        step("step 888888");
+    }
+
+    @Test
+    public void simpleTestFive() {
+        step("step 77777");
+        step("step 888888");
+    }
+}
+
