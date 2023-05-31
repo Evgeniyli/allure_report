c2d56c0404b512c96eba3b3f4388f6c1d7e00adf
Yevhen <Evgeniy.li2016@gmail.com>
2023-06-01 01:11:19 +0300

    fixes - 00000

--- src/test/java/com/demo/testframework/LoginTest.java
+++ src/test/java/com/demo/testframework/LoginTest.java
@@ -39,7 +39,7 @@ public class LoginTest extends BaseTest {
     @Issue("https://test2.com")
     @Description("Navigate to main page")
     @Severity(SeverityLevel.CRITICAL)
-    public void testCase_login9999TestProd() {
+    public void testCase_login000000TestProd() {
         TestReporter.reportDebugStep("Username has been logged in : %s ", "fdf");
     }
 
